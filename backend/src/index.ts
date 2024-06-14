436b8e0a721d9be31bb2c2bcc3a79e100c8a2d20
Lokeshc123 <lokeshchauhan629@gmail.com>
2024-06-14 14:01:34 +0530

    Intial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
